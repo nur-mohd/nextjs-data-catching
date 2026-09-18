de30cdba655c8badac9cefabb2340bb4915b475b
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-18 20:54:51 +0600

    3 Rules  Fetching Data & With try catch

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
+const getPosts = async () => {
+    const res = await fetch ("https://jsonplaceholder.typicode.com/posts");
+    return res.json();
+}
+
+const getPosts2 = async () => {
+    try {
+        const res =  await fetch("https://jsonplaceholder.typicode.com/posts");
+        return res.json();
+    } catch (error) {
+        throw new Error("Failed to fetch posts");
+    }
+}
+
+
+const getPosts3 = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if(!res.ok) {
+        throw new Error("Failed to fetch posts");
+    }
+    return res.json();
+}
+
+
+
 
 const PostPage = async() => {
 
-    const res =await fetch("https://jsonplaceholder.typicode.com/posts");
-    const posts = await res.json();
+   const posts = await getPosts(); // getPosts theke data fetch korar por posts variable e store kora holo.
 
+   console.log(posts);
 
     return (
         <div>
