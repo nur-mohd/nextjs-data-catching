3ba355370d36468879bef53e0d7e7d8abf4d293e
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-18 19:47:56 +0600

    Data fetch and Loading ...

---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostLoading = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold">Loading...</h2>
+        </div>
+    );
+};
+
+export default PostLoading;
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const PostPage = () => {
+
+const PostPage = async() => {
+
+    const res =await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await res.json();
+
+
     return (
         <div>
-            <h1 className="text-2xl font-bold">Hello, Posts</h1>
+            <h1 className="text-2xl font-bold">Hello, Posts {posts.length}</h1>
         </div>
     );
 };
