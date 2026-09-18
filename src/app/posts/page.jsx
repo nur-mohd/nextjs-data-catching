23c625ca308897646c47746653b571d94264b85f
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-18 17:50:51 +0600

    first commit

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostPage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Hello, Posts</h1>
+        </div>
+    );
+};
+
+export default PostPage;
