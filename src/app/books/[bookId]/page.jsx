8cb94a82dc4ee7c62c59ae3580e2fb64cf74ac46
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-19 14:39:59 +0600

    Suspense and Streaming with Data Fetching

--- src/app/books/[bookId]/page.jsx
+++ src/app/books/[bookId]/page.jsx
@@ -4,8 +4,7 @@ export const generateStaticParams = async () => {
     const res = await fetch("http://localhost:5000/books");
     const books = await res.json();
 
-    // return books.map((book) => ({ bookId: book.id}))
-    return books.slice(0,3).map((book) => ({ bookId: book.id}))
+    return books.map((book) => ({ bookId: book.id}))
 }
 
 
